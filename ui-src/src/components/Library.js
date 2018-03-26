204c3120299e6ebed6865fc62aaae4c1cba4ea2b
Connor Turland <connorturland@gmail.com>
2018-03-26 17:54:53 +0200

    break library into rows with columns of four

--- ui-src/src/components/Library.js
+++ ui-src/src/components/Library.js
@@ -13,14 +13,42 @@ class Library extends Component {
   }
 
   render = () => {
+    const arrays = [], size = 4
+    this.props.books.unshift('new')
+    while (this.props.books.length > 0)
+      arrays.push(this.props.books.splice(0, size))
+
+    return (
+      <div>
+        {arrays.map((set, i) => <LibraryRow first={i === 0} books={set} key={i} />)}
+      </div>
+    )
+  }
+}
+
+class LibraryRow extends Component {
+  render = () => {
     return (
       <div className="row">
-        <div className="col-3 book-image">
-          <Link to="/new" className="inner-book-image new-book">
-            Add Book
-          </Link>
-        </div>
-        {this.props.books.map((b, i) => <Book key={i} book={b} />)}
+        {this.props.books.map((b, i) => {
+          if (this.props.first && i === 0) {
+            return <NewBook key={i} />
+          } else {
+            return <Book key={i} book={b} />
+          }
+        })}
+      </div>
+    )
+  }
+}
+
+class NewBook extends Component {
+  render = () => {
+    return (
+      <div className="col-3 book-image">
+        <Link to="/new" className="inner-book-image new-book">
+          Add Book
+        </Link>
       </div>
     )
   }
